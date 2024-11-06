4078346040727efa706a1e106eea6dffb84ce4e2
ElingeMisa <110919618+elingemisa@users.noreply.github.com>
2024-11-06 14:09:38 -0600

    Probando probando

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,7 @@ dependencies{
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.navigation.compose)
 
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
