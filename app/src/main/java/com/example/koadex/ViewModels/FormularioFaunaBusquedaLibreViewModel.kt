18efdc499e6bb3e10fe94201b72687a698151375
Axel Ariel Grande Ruiz <shadow1234f@outlook.com>
2024-11-19 15:59:54 -0600

    Clase ViewModel para FormularioFaunaBusquedaLibre

---
+++ app/src/main/java/com/example/koadex/ViewModels/FormularioFaunaBusquedaLibreViewModel.kt
@@ -0,0 +1,73 @@
+package com.example.koadex.ViewModels
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class FormularioFaunaBusquedaLibreViewModel: ViewModel() {
+
+    @Composable
+    public fun AlturaButton(text: String, isSelected: Boolean, onClick: () -> Unit) {
+        Text(
+            text = text,
+            modifier = Modifier
+                .background(if (isSelected) Color(0xFF97B96E) else Color.Transparent)
+                .padding(8.dp)
+                .clickable(onClick = onClick),
+            color = if (isSelected) Color.Black else Color.Gray
+        )
+    }
+
+    @Composable
+    public fun ZonaButton(text: String, isSelected: Boolean, iconRes: Int, onClick: () -> Unit) {
+        Image(
+            painter = painterResource(id = iconRes),
+            contentDescription = text,
+            modifier = Modifier
+                .size(80.dp)
+                .aspectRatio(1.2f)
+                .background(if (isSelected) Color(0xFF97B96E) else Color.Transparent)
+                .padding(2.dp)
+                .clickable(onClick = onClick)
+        )
+    }
+
+    @Composable
+    public fun TipoAnimalButton(text: String, isSelected: Boolean, iconRes: Int, onClick: () -> Unit) {
+        Image(
+            painter = painterResource(id = iconRes),
+            contentDescription = text,
+            modifier = Modifier
+                .size(75.dp)
+                .background(if (isSelected) Color(0xFF97B96E) else Color.Transparent)
+                .padding(4.dp)
+                .clickable(onClick = onClick)
+        )
+    }
+
+    @Composable
+    public fun TipoObservacionButton(text: String, isSelected: Boolean, iconRes: Int, onClick: () -> Unit) {
+        Image(
+            painter = painterResource(id = iconRes),
+            contentDescription = text,
+            modifier = Modifier
+                .size(64.dp)
+                .background(if (isSelected) Color(0xFF97B96E) else Color.Transparent)
+                .padding(4.dp)
+                .clickable(onClick = onClick)
+        )
+    }
+
+}
--- app/src/main/java/com/example/koadex/Views/FormularioFaunaBusquedaLibre.kt
+++ app/src/main/java/com/example/koadex/Views/FormularioFaunaBusquedaLibre.kt
@@ -27,7 +27,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.koadex.R
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-
+import com.example.koadex.ViewModels.FormularioFaunaBusquedaLibreViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,6 +39,7 @@ fun FormularioFaunaBusquedaLibre(navController: NavHostController, modifier: Mod
     var tipoObservacionSeleccionada by remember { mutableStateOf("") }
     val actionButtonColors = ButtonDefaults.buttonColors(containerColor = Color(0xFF4E7029))
 
+    val FaunaBViewModel = FormularioFaunaBusquedaLibreViewModel()
 
     //Estado de scroll
     val scrollState = rememberScrollState()
@@ -72,10 +73,10 @@ fun FormularioFaunaBusquedaLibre(navController: NavHostController, modifier: Mod
             // Zona
             Text("Zona", style = MaterialTheme.typography.titleMedium)
             Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                ZonaButton("Bosque", zonaSeleccionada == "Bosque", R.drawable.ic_bosque) { zonaSeleccionada = "Bosque" }
-                ZonaButton("Arreglo Agroforestal", zonaSeleccionada == "Arreglo Agroforestal", R.drawable.ic_agroforestal) { zonaSeleccionada = "Arreglo Agroforestal" }
-                ZonaButton("Cultivos Transitorios", zonaSeleccionada == "Cultivos Transitorios", R.drawable.ic_cultivostransitorios) { zonaSeleccionada = "Cultivos Transitorios" }
-                ZonaButton("Cultivos Permanentes", zonaSeleccionada == "Cultivos Permanentes", R.drawable.ic_cultivospermanentes) { zonaSeleccionada = "Cultivos Permanentes" }
+                FaunaBViewModel.ZonaButton("Bosque", zonaSeleccionada == "Bosque", R.drawable.ic_bosque) { zonaSeleccionada = "Bosque" }
+                FaunaBViewModel.ZonaButton("Arreglo Agroforestal", zonaSeleccionada == "Arreglo Agroforestal", R.drawable.ic_agroforestal) { zonaSeleccionada = "Arreglo Agroforestal" }
+                FaunaBViewModel.ZonaButton("Cultivos Transitorios", zonaSeleccionada == "Cultivos Transitorios", R.drawable.ic_cultivostransitorios) { zonaSeleccionada = "Cultivos Transitorios" }
+                FaunaBViewModel.ZonaButton("Cultivos Permanentes", zonaSeleccionada == "Cultivos Permanentes", R.drawable.ic_cultivospermanentes) { zonaSeleccionada = "Cultivos Permanentes" }
             }
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -83,11 +84,11 @@ fun FormularioFaunaBusquedaLibre(navController: NavHostController, modifier: Mod
             // Tipo de Animal
             Text("Tipo de Animal", style = MaterialTheme.typography.titleMedium)
             Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                TipoAnimalButton("Mamífero", tipoAnimalSeleccionado == "Mamífero", R.drawable.ic_mamifero) { tipoAnimalSeleccionado = "Mamífero" }
-                TipoAnimalButton("Ave", tipoAnimalSeleccionado == "Ave", R.drawable.ic_ave) { tipoAnimalSeleccionado = "Ave" }
-                TipoAnimalButton("Reptil", tipoAnimalSeleccionado == "Reptil", R.drawable.ic_reptil) { tipoAnimalSeleccionado = "Reptil" }
-                TipoAnimalButton("Anfibio", tipoAnimalSeleccionado == "Anfibio", R.drawable.ic_anfibio) { tipoAnimalSeleccionado = "Anfibio" }
-                TipoAnimalButton("Insecto", tipoAnimalSeleccionado == "Insecto", R.drawable.ic_insecto) { tipoAnimalSeleccionado = "Insecto" }
+                FaunaBViewModel.TipoAnimalButton("Mamífero", tipoAnimalSeleccionado == "Mamífero", R.drawable.ic_mamifero) { tipoAnimalSeleccionado = "Mamífero" }
+                FaunaBViewModel.TipoAnimalButton("Ave", tipoAnimalSeleccionado == "Ave", R.drawable.ic_ave) { tipoAnimalSeleccionado = "Ave" }
+                FaunaBViewModel.TipoAnimalButton("Reptil", tipoAnimalSeleccionado == "Reptil", R.drawable.ic_reptil) { tipoAnimalSeleccionado = "Reptil" }
+                FaunaBViewModel.TipoAnimalButton("Anfibio", tipoAnimalSeleccionado == "Anfibio", R.drawable.ic_anfibio) { tipoAnimalSeleccionado = "Anfibio" }
+                FaunaBViewModel.TipoAnimalButton("Insecto", tipoAnimalSeleccionado == "Insecto", R.drawable.ic_insecto) { tipoAnimalSeleccionado = "Insecto" }
             }
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -135,11 +136,11 @@ fun FormularioFaunaBusquedaLibre(navController: NavHostController, modifier: Mod
             // Tipo de Observación
             Text("Tipo de Observación", style = MaterialTheme.typography.titleMedium)
             Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                TipoObservacionButton("La vio", tipoObservacionSeleccionada == "La vio", R.drawable.ic_la_vio) { tipoObservacionSeleccionada = "La vio" }
-                TipoObservacionButton("Huella", tipoObservacionSeleccionada == "Huella", R.drawable.ic_huella) { tipoObservacionSeleccionada = "Huella" }
-                TipoObservacionButton("Rastro", tipoObservacionSeleccionada == "Rastro", R.drawable.ic_rastro) { tipoObservacionSeleccionada = "Rastro" }
-                TipoObservacionButton("Cacería", tipoObservacionSeleccionada == "Cacería", R.drawable.ic_caceria) { tipoObservacionSeleccionada = "Cacería" }
-                TipoObservacionButton("Les Dijeron", tipoObservacionSeleccionada == "Les Dijeron", R.drawable.ic_les_dijeron) { tipoObservacionSeleccionada = "Les Dijeron" }
+                FaunaBViewModel.TipoObservacionButton("La vio", tipoObservacionSeleccionada == "La vio", R.drawable.ic_la_vio) { tipoObservacionSeleccionada = "La vio" }
+                FaunaBViewModel.TipoObservacionButton("Huella", tipoObservacionSeleccionada == "Huella", R.drawable.ic_huella) { tipoObservacionSeleccionada = "Huella" }
+                FaunaBViewModel.TipoObservacionButton("Rastro", tipoObservacionSeleccionada == "Rastro", R.drawable.ic_rastro) { tipoObservacionSeleccionada = "Rastro" }
+                FaunaBViewModel.TipoObservacionButton("Cacería", tipoObservacionSeleccionada == "Cacería", R.drawable.ic_caceria) { tipoObservacionSeleccionada = "Cacería" }
+                FaunaBViewModel.TipoObservacionButton("Les Dijeron", tipoObservacionSeleccionada == "Les Dijeron", R.drawable.ic_les_dijeron) { tipoObservacionSeleccionada = "Les Dijeron" }
             }
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -147,9 +148,9 @@ fun FormularioFaunaBusquedaLibre(navController: NavHostController, modifier: Mod
             // Altura de Observación
             Text("Altura de Observación", style = MaterialTheme.typography.titleMedium)
             Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                AlturaButton("< 1mt Baja", alturaObservacion == "< 1mt Baja") { alturaObservacion = "< 1mt Baja" }
-                AlturaButton("1-3 mt Media", alturaObservacion == "1-3 mt Media") { alturaObservacion = "1-3 mt Media" }
-                AlturaButton(">3mt Alta", alturaObservacion == ">3mt Alta") { alturaObservacion = ">3mt Alta" }
+                FaunaBViewModel.AlturaButton("< 1mt Baja", alturaObservacion == "< 1mt Baja") { alturaObservacion = "< 1mt Baja" }
+                FaunaBViewModel.AlturaButton("1-3 mt Media", alturaObservacion == "1-3 mt Media") { alturaObservacion = "1-3 mt Media" }
+                FaunaBViewModel.AlturaButton(">3mt Alta", alturaObservacion == ">3mt Alta") { alturaObservacion = ">3mt Alta" }
             }
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -190,60 +191,6 @@ fun FormularioFaunaBusquedaLibre(navController: NavHostController, modifier: Mod
             }
         }
     }
-}
-
-
-
-@Composable
-fun AlturaButton(text: String, isSelected: Boolean, onClick: () -> Unit) {
-    Text(
-        text = text,
-        modifier = Modifier
-            .background(if (isSelected) Color(0xFF97B96E) else Color.Transparent)
-            .padding(8.dp)
-            .clickable(onClick = onClick),
-        color = if (isSelected) Color.Black else Color.Gray
-    )
-}
-
-@Composable
-fun ZonaButton(text: String, isSelected: Boolean, iconRes: Int, onClick: () -> Unit) {
-    Image(
-        painter = painterResource(id = iconRes),
-        contentDescription = text,
-        modifier = Modifier
-            .size(80.dp)
-            .aspectRatio(1.2f)
-            .background(if (isSelected) Color(0xFF97B96E) else Color.Transparent)
-            .padding(2.dp)
-            .clickable(onClick = onClick)
-    )
-}
-
-@Composable
-fun TipoAnimalButton(text: String, isSelected: Boolean, iconRes: Int, onClick: () -> Unit) {
-    Image(
-        painter = painterResource(id = iconRes),
-        contentDescription = text,
-        modifier = Modifier
-            .size(75.dp)
-            .background(if (isSelected) Color(0xFF97B96E) else Color.Transparent)
-            .padding(4.dp)
-            .clickable(onClick = onClick)
-    )
-}
-
-@Composable
-fun TipoObservacionButton(text: String, isSelected: Boolean, iconRes: Int, onClick: () -> Unit) {
-    Image(
-        painter = painterResource(id = iconRes),
-        contentDescription = text,
-        modifier = Modifier
-            .size(64.dp)
-            .background(if (isSelected) Color(0xFF97B96E) else Color.Transparent)
-            .padding(4.dp)
-            .clickable(onClick = onClick)
-    )
 }
 
 
