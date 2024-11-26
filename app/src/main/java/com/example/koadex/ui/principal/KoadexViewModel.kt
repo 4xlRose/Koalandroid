f83f9f08046375c22300efc3b99b7af110c81f06
uwelOriginal <hgperezromero@outlook.com>
2024-11-26 15:25:01 -0600

    Koadex

--- app/src/main/java/com/example/koadex/ui/principal/KoadexViewModel.kt
+++ app/src/main/java/com/example/koadex/ui/principal/KoadexViewModel.kt
@@ -5,36 +5,36 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.koadex.data.FormRepository
 import com.example.koadex.data.GeneralFormEntity
+import com.example.koadex.data.SeasonEntity
+import com.example.koadex.data.WeatherEntity
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
-class KoadexViewModel(formsRespository: FormRepository) : ViewModel() {
-
-
+class KoadexViewModel(formsRepository: FormRepository) : ViewModel() {
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
 
     val koadexGeneralUiState: StateFlow<KoadexGeneralUiState> =
-        formsRespository.getAllForms().map { KoadexGeneralUiState(it) }
+        formsRepository.getAllForms().map { KoadexGeneralUiState(it) }
             .stateIn(
                 scope = viewModelScope,
                 started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
                 initialValue = KoadexGeneralUiState()
             )
+
+    val getAllUsers = formsRepository.getAllUsers()
+    val getAllFormStates = formsRepository.getAllFormStates()
+
+    val getWeatherById: (Int) -> Flow<WeatherEntity?> = { id ->
+        formsRepository.getWeatherById(id)
+    }
+    val getSeasonById: (Int) -> Flow<SeasonEntity?> = { id ->
+        formsRepository.getSeasonById(id)
+    }
 }
 
-data class KoadexUiState(val koadexList: List<GeneralFormEntity> = listOf()){
-
-}
-
-data class KoadexGeneralUiState(val koadexGeneralList: List<GeneralFormEntity> = listOf()){
-}
-
-interface FormRepository {
-    fun getAllForms(): Flow<List<GeneralFormEntity>>
-    fun getAllGeneralForms(): Flow<List<GeneralFormEntity>>
-}
+data class KoadexGeneralUiState(val koadexGeneralList: List<GeneralFormEntity> = listOf())
